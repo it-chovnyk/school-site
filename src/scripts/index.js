53cce613e6484c97a22851413f85b6a741252922
kkulaksyz <web4uvak@gmail.com>
2021-07-19 10:05:33 +0300

    Add mobile menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const $navToggleBtn = document.querySelector('.nav-toggle');
+const $navList = document.querySelector('.nav-list');
+
+$navToggleBtn.addEventListener('click', ({ target }) => {
+  target.classList.toggle('is-active');
+});
+$navList.addEventListener('click', ({ target }) => {
+  if (target.tagName === 'A') {
+    $navToggleBtn.classList.remove('is-active');
+  }
+});
