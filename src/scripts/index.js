d404a064356d25250d268be81a4f5dc77323ac77
kkulaksyz <web4uvak@gmail.com>
2021-08-07 13:04:47 +0300

    Add story page markup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,10 +3,10 @@
 const $navToggleBtn = document.querySelector('.nav-toggle');
 const $navList = document.querySelector('.nav-list');
 
-$navToggleBtn.addEventListener('click', ({ target }) => {
+$navToggleBtn?.addEventListener('click', ({ target }) => {
   target.classList.toggle('is-active');
 });
-$navList.addEventListener('click', ({ target }) => {
+$navList?.addEventListener('click', ({ target }) => {
   if (target.tagName === 'A') {
     $navToggleBtn.classList.remove('is-active');
   }
